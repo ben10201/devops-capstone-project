313963503ed763242ed4d656781be45d19311e6e
bn10201 <bennyfrey.mug@hotmail.com>
2023-11-19 12:50:00 -0500

    add update account feature

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,6 +138,22 @@ class TestAccountService(TestCase):
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
+
+    def test_update_account(self):
+        """It should update an existing account"""
+        # create an account to update
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = response.get_json()
+        new_account["name"] = "Whatever Human Name"
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        update_account = response.get_json()
+        self.assertEqual(update_account["name"], "Whatever Human Name")
+
     # ADD YOUR TEST CASES HERE ...
 
 
