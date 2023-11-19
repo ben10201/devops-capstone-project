c9ea6bc5680ab0aa9c888a63137e133997eec24b
bn10201 <bennyfrey.mug@hotmail.com>
2023-11-19 14:08:07 -0500

    add test to test method not allowed

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -168,4 +168,7 @@ class TestAccountService(TestCase):
         data = response.get_json()
         self.assertEqual(len(data), 5)
 
-
+    def test_method_not_allowed(self):
+        """It should not allow an illegal method call"""
+        resp = self.client.delete(BASE_URL)
+        self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
