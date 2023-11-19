87bebd7eba9d0181474c353971882f550e896025
bn10201 <bennyfrey.mug@hotmail.com>
2023-11-19 13:48:27 -0500

    add feature list all accounts

--- service/routes.py
+++ service/routes.py
@@ -61,7 +61,21 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
-# ... place you code here to LIST accounts ...
+@app.route("/accounts", methods=["GET"])
+def list_all_accounts():
+    """
+    List all accounts
+    This endpoint will list all accounts
+    """
+
+    app.logger.info("Request to list accounts")
+
+    accounts = Account.all()
+    account_list = [account.serialize() for account in accounts]
+
+    app.logger.info("Returning [%s] accounts", len(account_list))
+
+    return jsonify(account_list), status.HTTP_200_OK
 
 
 ######################################################################
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -154,12 +154,12 @@ class TestAccountService(TestCase):
         update_account = response.get_json()
         self.assertEqual(update_account["name"], "Whatever Human Name")
 
-    def test_delete_account(self):
-        """It should Delete an account"""
-        account = self._create_accounts(1)[0]
-        response = self.client.delete(f"{BASE_URL}/{account.id}")
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-
-    # ADD YOUR TEST CASES HERE ...
+    def test_list_all_account(self):
+        """It should list all accounts"""
+        self._create_accounts(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
 
 
