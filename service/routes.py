3e626c99844284d31c92e34c489112764b38b1d0
ben10201 <bennyfrey.mug@hotmail.com>
2023-11-20 07:13:18 -0500

    fix lint flake8 flags

--- service/routes.py
+++ service/routes.py
@@ -61,6 +61,7 @@ def create_accounts():
 # LIST ALL ACCOUNTS
 ######################################################################
 
+
 @app.route("/accounts", methods=["GET"])
 def list_all_accounts():
     """
@@ -109,11 +110,11 @@ def update_accounts(account_id):
     """
 
     app.logger.info("Request update an account with id: %s", account_id)
-    
+
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
 
@@ -136,7 +137,7 @@ def delete_accounts(account_id):
     account = Account.find(account_id)
     if account:
         account.delete()
-    
+
     return "", status.HTTP_204_NO_CONTENT
 
 
